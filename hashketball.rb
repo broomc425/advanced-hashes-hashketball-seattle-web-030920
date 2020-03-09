1533b1f0bb22dffff180f33d26a06f5e465182de
Charlie Broom <broomc425@gmail.com>
2020-03-09 15:56:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,6 +139,13 @@ def team_names
 end
 
 def player_numbers(team_name)
+  game_hash.each do |_place, team|
+    next unless team[:team_name] == team_name
+    team.each do |attribute, data|
+      next unless attribute == :players
+      data.each do |data|
+        data << player[:number]
+        
 end
 
 def player_stats(sought_player_name)
