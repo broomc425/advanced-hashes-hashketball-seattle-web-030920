eba7cf18cbabe133798eb54f594cc8f673714f9f
Charlie Broom <broomc425@gmail.com>
2020-03-09 15:53:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -106,7 +106,13 @@ end
 
 def num_points_scored(sought_player_name)
   game_hash.each do |_place, team|
-    team.each do |
+    team.each do |attribute, data|
+      next unless attribute == :players
+      data.each do |player|
+        return player[:points] if player[:player_name] == sought_player_name
+      end
+    end
+  end
 end
 
 def shoe_size(sought_player_name)
